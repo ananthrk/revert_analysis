cd5629458b6697c9d4ef66cfbc9bef507dfe697a
AnanthRK <ananthrk@gmail.com>
2015-03-27 19:03:32 +0530

    Introduced new method for revert statistics

--- reverts.py
+++ reverts.py
@@ -67,13 +67,106 @@ def main():
     else:
         end = start + (60 * 60 * 24) #1 day
 
-    for revert in get_reverts(start, end, args.revert_radius, args.revert_window):
+    for revert in get_reverts(start, end, args.revert_radius, 
+            args.revert_window):
         print(revert)
 
-def get_reverts(start, end, revert_radius=DEFAULT_REVERT_RADIUS, revert_window=DEFAULT_REVERT_WINDOW):
+    stats = get_revision_stats(start, end, args.revert_radius, 
+        args.revert_window)
+    print(stats)
+
+
+def get_revision_stats(start, end, revert_radius=DEFAULT_REVERT_RADIUS, 
+        revert_window=DEFAULT_REVERT_WINDOW):
     """
-    Analyzes the list of revisions between `start` and `end` time range to detect any identical reverts
-    that happened in the `revert_window` within `revert_radius` revisions
+    Computes the total number of revisions and total number of reverts for 
+    each page edited between `start` and `end`
+
+    :Parameters:
+        start : `Timestamp`
+            Start timestamp to look for reverting edits
+        end : `Timestamp`
+            End timestamp to look for reverting edits
+        revert_radius : int
+            Maximum number of revisions to consider for a revert to occur
+        revert_window : int
+            Maximum age in seconds of a ""reverted to"" edit
+
+    :Return:
+        a dictionary with `rev_page` as key and the tuple 
+        (total_revisions, total_reverts) as value
+    """
+    if not start:
+        raise ValueError("Invalid start")
+    if not end:
+        raise ValueError("Invalid end")
+    edit_window_start = start - revert_window
+
+    query = """
+        SELECT r1.rev_id, r1.rev_page, r1.rev_sha1, r1.rev_timestamp 
+        FROM revision r1 
+        WHERE 
+        r1.rev_timestamp >= %s AND r1.rev_timestamp <= %s 
+        ORDER BY r1.rev_page, r1.rev_timestamp ASC;
+    """
+
+    values = [edit_window_start.short_format(), end.short_format()]
+
+    global db
+    #cursor = db.shared_connection.cursor()
+    #cursor.execute(query, values)
+
+    #FIXME: Just some test values until we get actual values from DB
+    cursor = [
+        {'rev_id': 653552189, 'rev_page': 46229350, 
+            'rev_sha1' : 'rhuxsljucr65exl77pb9nree4xi8lt0', 
+            'rev_timestamp' : '20150325230000'},
+        {'rev_id': 653552612, 'rev_page': 46229350, 
+            'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4', 
+            'rev_timestamp' : '20150326000000'},
+        {'rev_id': 653552809, 'rev_page': 46229350, 
+            'rev_sha1' : 'ibhyl01ikzp0n3ss0egb0kdqse5diq1', 
+            'rev_timestamp' : '20150326010000'},
+        {'rev_id': 653553137, 'rev_page': 46229350, 
+            'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4', 
+            'rev_timestamp' : '20150326020000'},
+        {'rev_id': 653553138, 'rev_page': 47898909, 
+            'rev_sha1' : 'zzzc7qgbiunuj1sxz1wmuyaflxwzzzz', 
+            'rev_timestamp' : '20150326030000'}
+    ]
+
+    revert_stats = {}
+    detector = reverts.Detector(revert_radius)
+    
+    page_revisions = groupby(cursor, key=lambda r:r['rev_page'])
+    for page_id, revisions in page_revisions:
+
+        total_revisions = 0
+        total_reverts = 0
+
+        for r in revisions:     
+            is_in_range = (Timestamp(r['rev_timestamp']) >= start and \
+                Timestamp(r['rev_timestamp']) <= end)     
+            revert = detector.process(r['rev_sha1'], r)     
+            is_revert = revert and \
+                Timestamp(revert.reverting['rev_timestamp']) >= start and \
+                Timestamp(revert.reverting['rev_timestamp']) <= end     
+            if is_in_range:
+                total_revisions = total_revisions + 1     
+            if is_revert: 
+                total_reverts = total_reverts + 1
+
+        revert_stats[page_id] = (total_revisions, total_reverts)
+
+    return revert_stats
+
+
+def get_reverts(start, end, revert_radius=DEFAULT_REVERT_RADIUS, 
+        revert_window=DEFAULT_REVERT_WINDOW):
+    """
+    Analyzes the list of revisions between `start` and `end` time range to 
+    detect any identical reverts that happened in the `revert_window` 
+    within `revert_radius` revisions
 
     :Parameters:
         start : `Timestamp`
@@ -95,7 +188,8 @@ def get_reverts(start, end, revert_radius=DEFAULT_REVERT_RADIUS, revert_window=D
     edit_window_start = start - revert_window
 
     query = """
-        SELECT r1.rev_id, r1.rev_page, r1.rev_sha1, r1.rev_timestamp FROM revision r1 
+        SELECT r1.rev_id, r1.rev_page, r1.rev_sha1, r1.rev_timestamp 
+        FROM revision r1 
         WHERE 
         r1.rev_timestamp >= %s AND r1.rev_timestamp <= %s 
         ORDER BY r1.rev_page, r1.rev_timestamp ASC;
@@ -109,18 +203,28 @@ def get_reverts(start, end, revert_radius=DEFAULT_REVERT_RADIUS, revert_window=D
 
     #FIXME: Just some test values until we get to retrieve actual values from DB
     cursor = [
-        {'rev_id': 653552189, 'rev_page': 46229350, 'rev_sha1' : 'rhuxsljucr65exl77pb9nree4xi8lt0'},
-        {'rev_id': 653552612, 'rev_page': 46229350, 'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4'},
-        {'rev_id': 653552809, 'rev_page': 46229350, 'rev_sha1' : 'ibhyl01ikzp0n3ss0egb0kdqse5diq1'},
-        {'rev_id': 653553137, 'rev_page': 46229350, 'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4'},
-        {'rev_id': 653553138, 'rev_page': 47898909, 'rev_sha1' : 'zzzc7qgbiunuj1sxz1wmuyaflxwzzzz'}
+        {'rev_id': 653552189, 'rev_page': 46229350, 
+            'rev_sha1' : 'rhuxsljucr65exl77pb9nree4xi8lt0', 
+            'rev_timestamp' : '20150325230000'},
+        {'rev_id': 653552612, 'rev_page': 46229350, 
+            'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4', 
+            'rev_timestamp' : '20150326000000'},
+        {'rev_id': 653552809, 'rev_page': 46229350, 
+            'rev_sha1' : 'ibhyl01ikzp0n3ss0egb0kdqse5diq1', 
+            'rev_timestamp' : '20150326010000'},
+        {'rev_id': 653553137, 'rev_page': 46229350, 
+            'rev_sha1' : 'bxzc7qgbiunuj1sxz1wmuyaflxwa3t4', 
+            'rev_timestamp' : '20150326020000'},
+        {'rev_id': 653553138, 'rev_page': 47898909, 
+            'rev_sha1' : 'zzzc7qgbiunuj1sxz1wmuyaflxwzzzz', 
+            'rev_timestamp' : '20150326030000'}
     ]
 
     current_page = None #pointer to current page being processed
     
     page_revisions = groupby(cursor, key=lambda r:r['rev_page'])
     for page_id, revisions in page_revisions:
-        
+
         for revert in reverts.detect((r['rev_sha1'], r) for r in revisions):
             if Timestamp(revert.reverting['rev_timestamp']) >= start and \
                Timestamp(revert.reverting['rev_timestamp']) <= end:
